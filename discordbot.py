65525ad98c346e931492d17dd2f07876ac216c48
takuya-fujimura24 <78469673+takuya-fujimura24@users.noreply.github.com>
2021-02-04 12:00:04 +0900

    implement hello

--- discordbot.py
+++ discordbot.py
@@ -17,5 +17,8 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
+@bot.command()
+async def hello(ctx):
+    await ctx.send('おはようございます! 優木せつ菜です‼')
 
 bot.run(token)
